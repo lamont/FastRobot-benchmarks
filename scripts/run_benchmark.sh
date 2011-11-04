044933d505790d5b25648688b9ec08d961ac75a4
Lamont Lucas <lamont@fastrobot.com>
2011-11-04 03:16:29 -0400

    Benchmarks run for all the -standalone cases

--- scripts/run_benchmark.sh
+++ scripts/run_benchmark.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-for I in 1 2 3 4 5; do sysbench --num-threads=32 --max-requests=0 --max-time=120 --test=oltp --db-driver=mysql --mysql-host=ip-10-110-66-14.ec2.internal --oltp-table-size=40000000 run | tee ec2-galera/percona-standalone/2min_run_no_$I.out; done
+for I in 1 2 3 4 5; do sysbench --num-threads=32 --max-requests=0 --max-time=120 --test=oltp --db-driver=mysql --mysql-host=ip-10-110-66-14.ec2.internal --oltp-table-size=40000000 run | tee ec2-galera/vanilla-mysql-standalone/2min_run_no_$I.out; done
