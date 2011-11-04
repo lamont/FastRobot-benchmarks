a41f8f1b175b50734a6f30050bd225ef9c0466d1
Lamont Lucas <lamont@fastrobot.com>
2011-11-04 02:22:08 -0400

    added percona-standalone test for Percona-Server-5.5.16-rel22.0-189 managed to use the same mysql store, which is neat

--- scripts/run_benchmark.sh
+++ scripts/run_benchmark.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-for I in 1 2 3 4 5; do sysbench --num-threads=32 --max-requests=0 --max-time=120 --test=oltp --db-driver=mysql --mysql-host=domU-12-31-39-05-14-A1.compute-1.internal  --oltp-table-size=40000000 run | tee ec2-galera/standalone/2min_run_no_$I.out; done
+for I in 1 2 3 4 5; do sysbench --num-threads=32 --max-requests=0 --max-time=120 --test=oltp --db-driver=mysql --mysql-host=ip-10-110-66-14.ec2.internal --oltp-table-size=40000000 run | tee ec2-galera/percona-standalone/2min_run_no_$I.out; done
