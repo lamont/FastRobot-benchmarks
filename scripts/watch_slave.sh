2e8facb5ea84f2c22310d5f7d5582948ddc46c9a
Lamont Lucas <lamont@fastrobot.com>
2011-11-07 04:38:08 -0500

    re-ran percona benchmarks with tx-commit=2

--- scripts/watch_slave.sh
+++ scripts/watch_slave.sh
@@ -3,6 +3,6 @@
 while true
 do
 echo -n `date +%s`
-mysql -h domU-12-31-39-16-9C-F9.compute-1.internal -u root -e 'show slave status\G' |grep Seconds
+mysql -h rep2 -u root -e 'show slave status\G' |grep Seconds
 sleep 1
 done
