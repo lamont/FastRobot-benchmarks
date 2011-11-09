6cdde6369c23e2e5a1479bac582aaf80d5181815
Lamont Lucas <lamont@fastrobot.com>
2011-11-09 01:55:44 -0500

    more threads actually lets the slave fall behind

--- scripts/add_2_more_sysbench.sh
+++ scripts/add_2_more_sysbench.sh
@@ -4,4 +4,4 @@ OUTDIR=$1
 DURATION=$2
 
 
-sysbench --num-threads=2 --max-requests=0 --max-time=$DURATION --test=oltp --db-driver=mysql --mysql-host=rep1 --oltp-table-size=40000000 run | tee $OUTDIR/add_2_${DURATION}.out
+sysbench --num-threads=2 --max-requests=0 --max-time=$DURATION --test=oltp --oltp-dist-type=uniform --db-driver=mysql --mysql-host=rep1 --oltp-table-size=40000000 run | tee $OUTDIR/add_2_${DURATION}.out
